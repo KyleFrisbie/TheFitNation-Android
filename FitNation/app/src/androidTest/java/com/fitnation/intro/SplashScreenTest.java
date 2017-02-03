f698808cb66b7942bddd78f462373dde767d2b28
Ryan Newsom <rnewsom@msudenver.edu>
2017-02-02 17:57:56 -0700

    Added more tests

--- FitNation/app/src/androidTest/java/com/fitnation/intro/SplashScreenTest.java
+++ FitNation/app/src/androidTest/java/com/fitnation/intro/SplashScreenTest.java
@@ -1,9 +1,11 @@
-package com.fitnation;
+package com.fitnation.intro;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.fitnation.R;
 import com.fitnation.intro.SplashScreenActivity;
 
 import org.junit.Rule;
@@ -24,6 +26,6 @@ public class SplashScreenTest {
 
     @Test
     public void splashScreenIsDisplayed() {
-        onView(withText(R.string.title_activity_splash_screen)).check(matches(isDisplayed()));
+        onView(ViewMatchers.withText(R.string.title_activity_splash_screen)).check(matches(isDisplayed()));
     }
 }
--- FitNation/app/src/androidTest/java/com/fitnation/login/LoginScreenTest.java
+++ FitNation/app/src/androidTest/java/com/fitnation/login/LoginScreenTest.java
@@ -1,17 +1,18 @@
-package com.fitnation;
+package com.fitnation.login;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.fitnation.intro.SplashScreenActivity;
-import com.fitnation.login.LoginActivity;
+import com.fitnation.R;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -26,11 +27,15 @@ public class LoginScreenTest {
 
     @Test
     public void loginScreenIsDisplayed() {
-        onView(withText(R.string.loginScreen)).check(matches(isDisplayed()));
+        onView(ViewMatchers.withText(R.string.login_screen)).check(matches(isDisplayed()));
         onView(withId(R.id.login_button)).check(matches(isDisplayed()));
     }
 
-    
-
-
+    @Test
+    public void testMainActivityLaunchedUponLogin() {
+        onView(withId(R.id.login_button)).perform(click());
+        onView(withId(R.id.app_bar)).check(matches(isDisplayed()));
+        onView(withText(R.string.app_name)).check(matches(isDisplayed()));
+        onView(withId(R.id.drawer_layout)).check(matches(isDisplayed()));
+    }
 }
