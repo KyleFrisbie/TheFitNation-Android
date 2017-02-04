08ef80fc1d54789217194a2a00e4db162afaffd9
RNewsom <ryanjnewsom@gmail.com>
2017-02-03 21:39:03 -0700

    fixed project structure

---
+++ app/src/main/java/com/fitnation/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.fitnation.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * BaseActivity to be used by all Activities in the project
+ */
+public class BaseActivity extends AppCompatActivity {
+}
