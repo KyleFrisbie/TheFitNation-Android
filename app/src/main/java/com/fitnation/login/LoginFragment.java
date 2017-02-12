e7f74f52bdc6c2a95cdab7a81ff02482c491fe53
Ryan Newsom <rnewsom@msudenver.edu>
2017-02-11 18:05:40 -0700

    syncing master (#1)
    * Fixed travis
    
    * removed chmod
    
    * trying another template
    
    * test
    
    * removed gradle from gitignore
    
    * should do it
    
    * fixed project structure
    
    * fixed project structure
    
    * permission access for travis
    
    * fixed file format
    
    * regenerated wrapper
    
    * adding wwrapper
    
    * new gradle
    
    * test
    
    * edited gradle properties
    
    * no gradle wrapper call
    
    * test
    
    * test
    
    * Adding new wrapper
    
    * Adding new gradlew
    
    * added jar
    
    * updated gradle

---
+++ app/src/main/java/com/fitnation/login/LoginFragment.java
@@ -0,0 +1,75 @@
+package com.fitnation.login;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.fitnation.R;
+import com.fitnation.base.BaseActivity;
+import com.fitnation.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * View for login
+ */
+public class LoginFragment extends BaseFragment implements LoginContract.View {
+    private LoginContract.Presenter mPresenter;
+
+    @BindView(R.id.login_button) public Button mLoginButton;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mPresenter.start();
+    }
+
+    @OnClick(R.id.login_button)
+    public void onLoginButtonClicked() {
+        mPresenter.onFacebookLoginPressed();
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void showAuthError() {
+
+    }
+
+    @Override
+    public void setPresenter(LoginContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Override
+    public BaseActivity getBaseActivity() {
+        return (BaseActivity) getActivity();
+    }
+}
---
+++ app/src/main/java/com/fitnation/navigation/NavigationActivity.java
@@ -0,0 +1,96 @@
+package com.fitnation.navigation;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.fitnation.R;
+import com.fitnation.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Handles Navigation for the flyout drawer & its container
+ */
+public class NavigationActivity extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    @BindView(R.id.toolbar) public Toolbar mToolbar;
+    @BindView(R.id.drawer_layout) public DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        ButterKnife.bind(this);
+        setUpActionBar();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setUpActionBar() {
+        setSupportActionBar(mToolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_start_workout) {
+
+        } else if (id == R.id.nav_my_workouts) {
+
+        } else if (id == R.id.nav_trends) {
+
+        } else if (id == R.id.nav_workout_regimens) {
+
+        } else if (id == R.id.nav_my_profile) {
+
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
