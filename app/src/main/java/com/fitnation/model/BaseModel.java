b63ea7a11f05f1ff137795e79f5f27238d24c2b1
RNewsom <ryanjnewsom@gmail.com>
2017-02-25 12:11:36 -0700

    Added realm

--- app/src/main/java/com/fitnation/model/BaseModel.java
+++ app/src/main/java/com/fitnation/model/BaseModel.java
@@ -2,8 +2,11 @@ package com.fitnation.model;
 
 import java.io.Serializable;
 
+import io.realm.RealmObject;
+
 /**
  * All models extend this guy so that if we ever switch how we save data, it is easy
  */
-public class BaseModel implements Serializable{
+public class BaseModel extends RealmObject {
+    private Integer androidId;
 }
