78e5db697f57db7eb250cb53f0212fa14aa82a9d
RNewsom <ryanjnewsom@gmail.com>
2017-04-01 16:08:13 -0600

    Fix failing test by adding check to see if its prod or debug

--- app/src/test/java/com/fitnation/utils/EnvironmentManagerTest.java
+++ app/src/test/java/com/fitnation/utils/EnvironmentManagerTest.java
@@ -1,5 +1,7 @@
 package com.fitnation.utils;
 
+import com.fitnation.BuildConfig;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,13 +14,20 @@ public class EnvironmentManagerTest {
     public void getCurrentEnvironment() throws Exception {
         final String DEV_ENDPOINT = "https://the-fit-nation-dev.herokuapp.com/";
         final String DEV_API_ENDPOINT = "https://the-fit-nation-dev.herokuapp.com/api/";
+        final String PROD_URL = "http://www.the-fit-nation.com/";
+        final String PROD_API_URL = "http://www.the-fit-nation.com/api/";
         EnvironmentManager environmentManager = EnvironmentManager.getInstance();
 
         assertNotNull(environmentManager);
 
         Environment current = environmentManager.getCurrentEnvironment();
-        assertTrue(current.getBaseUrl().equals(DEV_ENDPOINT));
-        assertTrue(current.getApiUrl().equals(DEV_API_ENDPOINT));
+        if(BuildConfig.DEBUG) {
+            assertTrue(current.getBaseUrl().equals(DEV_ENDPOINT));
+            assertTrue(current.getApiUrl().equals(DEV_API_ENDPOINT));
+        } else {
+            assertTrue(current.getBaseUrl().equals(PROD_URL));
+            assertTrue(current.getApiUrl().equals(PROD_API_URL));
+        }
     }
 
     @Test
