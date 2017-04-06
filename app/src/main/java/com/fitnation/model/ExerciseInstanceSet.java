51303704b411b47e1c5371e8f5e34656c8cfed03
Ryan Newsom <rnewsom@msudenver.edu>
2017-04-06 14:02:22 -0600

    fixed tests

--- app/src/main/java/com/fitnation/model/ExerciseInstanceSet.java
+++ app/src/main/java/com/fitnation/model/ExerciseInstanceSet.java
@@ -104,4 +104,41 @@ public class ExerciseInstanceSet extends RealmObject implements Cloneable{
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ExerciseInstanceSet that = (ExerciseInstanceSet) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (androidId != null ? !androidId.equals(that.androidId) : that.androidId != null)
+            return false;
+        if (orderNumber != null ? !orderNumber.equals(that.orderNumber) : that.orderNumber != null)
+            return false;
+        if (repQuantity != null ? !repQuantity.equals(that.repQuantity) : that.repQuantity != null)
+            return false;
+        if (effortQuantity != null ? !effortQuantity.equals(that.effortQuantity) : that.effortQuantity != null)
+            return false;
+        if (restTime != null ? !restTime.equals(that.restTime) : that.restTime != null)
+            return false;
+        if (exerciseInstanceId != null ? !exerciseInstanceId.equals(that.exerciseInstanceId) : that.exerciseInstanceId != null)
+            return false;
+        return notes != null ? notes.equals(that.notes) : that.notes == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (androidId != null ? androidId.hashCode() : 0);
+        result = 31 * result + (orderNumber != null ? orderNumber.hashCode() : 0);
+        result = 31 * result + (repQuantity != null ? repQuantity.hashCode() : 0);
+        result = 31 * result + (effortQuantity != null ? effortQuantity.hashCode() : 0);
+        result = 31 * result + (restTime != null ? restTime.hashCode() : 0);
+        result = 31 * result + (exerciseInstanceId != null ? exerciseInstanceId.hashCode() : 0);
+        result = 31 * result + (notes != null ? notes.hashCode() : 0);
+        return result;
+    }
 }
