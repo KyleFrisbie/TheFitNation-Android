9e2cf8d5d31cec123429afa0c7b376ecc08d1885
o002073 <Ryan.J.Newsom@kp.org>
2017-04-30 12:03:40 -0600

    all tests pass

--- app/src/androidTest/java/com/fitnation/navigation/NavigationScreenTest.java
+++ app/src/androidTest/java/com/fitnation/navigation/NavigationScreenTest.java
@@ -81,7 +81,6 @@ public class NavigationScreenTest extends InstrumentationTest{
     public void onNavWorkoutRegimensPressed(){
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).check(matches(isDisplayed()));
-        onView(withText("My Workout Regimens")).perform(click());
     }
 
     public void onNavMyProfilePressed(){
